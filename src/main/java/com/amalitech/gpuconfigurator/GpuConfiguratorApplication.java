e63d4ca0cd242e38e09b0c9346ad8961009f6ee5
Joseph Ampadu <joseph.ampadu@amalitech.com>
2023-12-06 13:47:02 +0000

    Generate project

---
+++ src/main/java/com/amalitech/gpuconfigurator/GpuConfiguratorApplication.java
@@ -0,0 +1,13 @@
+package com.amalitech.gpuconfigurator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpuConfiguratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpuConfiguratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amalitech/gpuconfigurator/GpuConfiguratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalitech.gpuconfigurator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpuConfiguratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
