7ee499f5cec724df4c9933edede451579d7d170f
Dickson Anyaele <dickson.anyaele@amalitech.com>
2023-12-11 16:33:29 +0000

    ongoing: test email and otp controller

--- src/main/java/com/amalitech/gpuconfigurator/TestEmailOtpController.java
+++ src/main/java/com/amalitech/gpuconfigurator/TestEmailOtpController.java
@@ -8,7 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestEmailOtpController {
 
     /*
+    * take user request
+    * validate email
+    * create user
+    * generate token and assign to user
     *
+    *
+    * /endpoint to verify token
+    * get the token associated with user email
+    * compare token if valid token check User as isValid
+    * if valid token and correct taken make the user to be verified
+    * create jwt for user
+    * /get the email - find user
     *
     * */
 
