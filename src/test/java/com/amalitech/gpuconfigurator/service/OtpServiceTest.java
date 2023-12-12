e2edbe967fa67ae1c1d72b9287d2891fdcbf994f
Dickson Anyaele <dickson.anyaele@amalitech.com>
2023-12-12 09:47:13 +0000

    test: add OtpService test

--- src/test/java/com/amalitech/gpuconfigurator/service/OtpServiceTest.java
+++ src/test/java/com/amalitech/gpuconfigurator/service/OtpServiceTest.java
@@ -1,4 +1,24 @@
 package com.amalitech.gpuconfigurator.service;
 
+import com.amalitech.gpuconfigurator.service.otpService.OtpService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@RequiredArgsConstructor
 public class OtpServiceTest {
+
+    private final OtpService otpService;
+
+    @Test
+    public void generateOtp_shouldGenerateNonEmptyString() {
+        String otp = otpService.generateOtp();
+
+        assertNotNull(otp);
+        assertFalse(otp.isEmpty());
+    }
+
 }
