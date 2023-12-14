b4d5f0ac65f2482cd0bf64d5bc534165f1a4fc75
Dickson Anyaele <dickson.anyaele@amalitech.com>
2023-12-14 16:19:37 +0000

    refactor: remove email test

--- src/test/java/com/amalitech/gpuconfigurator/service/EmailServiceTest.java
+++ src/test/java/com/amalitech/gpuconfigurator/service/EmailServiceTest.java
@@ -1,25 +1,34 @@
 package com.amalitech.gpuconfigurator.service;
 
+import com.amalitech.gpuconfigurator.model.OtpType;
+import com.amalitech.gpuconfigurator.service.email.EmailService;
 import com.amalitech.gpuconfigurator.service.email.EmailServiceImpl;
+import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.context.Context;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class EmailServiceTest {
 
-   @Autowired
+
    @InjectMocks
    private EmailServiceImpl emailService;
 
@@ -29,9 +38,6 @@ public class EmailServiceTest {
     @Mock
     private ITemplateEngine templateEngine;
 
-    @Mock
-    private MimeMessage mimeMessage;
-
     @Test
     public void isValidEmail_shouldPassIfEmailIsValid() {
         String testEmail = "testemail@gmail.com";
@@ -39,29 +45,5 @@ public class EmailServiceTest {
 
         assertTrue(isValidEmail);
     }
-
-    /*
-    @Test
-    public void sendOtpMessage_shouldProcessTemplateAndSendMessage() throws MessagingException {
-        String to = "zomlelarke@gufum.com";
-        String otp = "123456";
-
-        when(templateEngine.process(anyString(), any(Context.class))).thenReturn("Processed template");
-
-        emailService.sendOtpMessage(to, otp);
-
-        verify(templateEngine).process(anyString(), any(Context.class));
-        verify(javaMailSender).send(any(MimeMessage.class));
-
-        // Implement additional verification/assertions for your email sending logic
-        ArgumentCaptor<MimeMessage> captor = ArgumentCaptor.forClass(MimeMessage.class);
-        verify(javaMailSender).send(captor.capture());
-
-        MimeMessage sentMessage = captor.getValue();
-        MimeMessageHelper helper = new MimeMessageHelper(sentMessage);
-        assertEquals("OTP password for GPU Configurator", helper.getMimeMessage().getSubject());
-    }
-
-     */
 
 }
