a9ef7d3bdc1fc394199d46602c1a72dc2d030609
Dickson Anyaele <dickson.anyaele@amalitech.com>
2023-12-28 09:11:11 +0000

    feat: added the get all product and get one product service

--- src/main/java/com/amalitech/gpuconfigurator/service/ProductServiceImpl.java
+++ src/main/java/com/amalitech/gpuconfigurator/service/ProductServiceImpl.java
@@ -3,19 +3,23 @@ package com.amalitech.gpuconfigurator.service;
 
 import com.amalitech.gpuconfigurator.dto.product.CreateProductResponseDto;
 import com.amalitech.gpuconfigurator.dto.product.ProductDto;
+import com.amalitech.gpuconfigurator.dto.product.ProductResponse;
 import com.amalitech.gpuconfigurator.model.Category;
 import com.amalitech.gpuconfigurator.model.Product;
 import com.amalitech.gpuconfigurator.repository.CategoryRepository;
 import com.amalitech.gpuconfigurator.repository.ProductRepository;
 import com.amalitech.gpuconfigurator.service.category.CategoryServiceImpl;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -57,6 +61,36 @@ public class ProductServiceImpl implements ProductService {
                 .createdAt(product.getCreatedAt())
                 .id(product.getId().toString())
                 .build();
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream()
+                .map(product -> ProductResponse.builder()
+                        .productName(product.getProductName())
+                        .id(product.getId().toString())
+                        .productId(product.getProductId())
+                        .productDescription(product.getProductDescription())
+                        .productPrice(BigDecimal.valueOf(product.getProductPrice()))
+                        .imageUrl(product.getImageUrl().toString())
+                        .category(product.getCategory())
+                        .build()).collect(Collectors.toList());
+    }
+
+    public ProductResponse getProduct(String id) {
+        Product product = productRepository.findById(UUID.fromString(id)).orElseThrow(() -> new EntityNotFoundException("product not found"));
+
+        return ProductResponse.builder()
+                .productName(product.getProductName())
+                .id(product.getId().toString())
+                .productId(product.getProductId())
+                .productDescription(product.getProductDescription())
+                .productPrice(BigDecimal.valueOf(product.getProductPrice()))
+                .imageUrl(product.getImageUrl().toString())
+                .category(product.getCategory())
+                .build();
+
     }
 
 
