9cfcf18ec41e6df9042c35ff7dd16fed72fcb617
Dickson Anyaele <dickson.anyaele@amalitech.com>
2024-01-02 11:29:41 +0000

    feat: added brand repository

---
+++ src/main/java/com/amalitech/gpuconfigurator/repository/BrandRepository.java
@@ -0,0 +1,9 @@
+package com.amalitech.gpuconfigurator.repository;
+
+import com.amalitech.gpuconfigurator.model.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BrandRepository extends JpaRepository<Brand, UUID> {
+}
