815c6c1a2e3d5b9877b792806e4a38d8c36d7100
Dickson Anyaele <dickson.anyaele@amalitech.com>
2024-01-02 16:13:57 +0000

    feat: added brand controller

--- src/main/java/com/amalitech/gpuconfigurator/service/brand/BrandService.java
+++ src/main/java/com/amalitech/gpuconfigurator/service/brand/BrandService.java
@@ -2,14 +2,20 @@ package com.amalitech.gpuconfigurator.service.brand;
 
 import com.amalitech.gpuconfigurator.dto.GenericResponse;
 import com.amalitech.gpuconfigurator.dto.brand.BrandDto;
+import com.amalitech.gpuconfigurator.dto.brand.BrandRequest;
+import com.amalitech.gpuconfigurator.model.Brand;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface BrandService {
-    BrandDto createBrand(BrandDto brandDto);
+
+    Brand createBrand(BrandRequest request);
 
     List<BrandDto> getAllBrands();
 
     GenericResponse deleteBrand(UUID id);
+
+    GenericResponse updatedBrand(UUID id, @NotNull BrandRequest request);
 }
--- src/main/java/com/amalitech/gpuconfigurator/service/brand/BrandServiceImpl.java
+++ src/main/java/com/amalitech/gpuconfigurator/service/brand/BrandServiceImpl.java
@@ -2,9 +2,11 @@ package com.amalitech.gpuconfigurator.service.brand;
 
 import com.amalitech.gpuconfigurator.dto.GenericResponse;
 import com.amalitech.gpuconfigurator.dto.brand.BrandDto;
+import com.amalitech.gpuconfigurator.dto.brand.BrandRequest;
 import com.amalitech.gpuconfigurator.model.Brand;
 import com.amalitech.gpuconfigurator.repository.BrandRepository;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,13 +20,13 @@ public class BrandServiceImpl implements BrandService {
     private final BrandRepository brandRepository;
 
     @Override
-    public BrandDto createBrand(BrandDto brandDto) {
+    public Brand createBrand(BrandRequest request) {
         Brand newBrand = Brand
                 .builder()
-                .name(brandDto.name())
+                .name(request.name())
                 .build();
 
-        return new BrandDto(newBrand.getName(), newBrand.getId().toString());
+        return brandRepository.save(newBrand);
     }
 
     @Override
@@ -40,8 +42,23 @@ public class BrandServiceImpl implements BrandService {
     @Override
     public GenericResponse deleteBrand(UUID id) {
         Brand brand = brandRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("brand cannot be found"));
-        brandRepository.deleteById(id);
+        brandRepository.deleteById(brand.getId());
         return new GenericResponse(204, "brand has been deleted successfully");
     }
+
+    @Override
+    public GenericResponse updatedBrand(UUID id, String name) {
+        return null;
+    }
+
+    @Override
+    public GenericResponse updatedBrand(UUID id, @NotNull BrandRequest request) {
+        Brand brand = brandRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("brand cannot be found"));
+        brand.setName(request.name());
+
+        brandRepository.save(brand);
+        return new GenericResponse(201, "brand has been updated successfully");
+    }
+
 
 }
