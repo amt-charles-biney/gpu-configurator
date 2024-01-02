8efe8f70f931453edff1d19db0c8281939886013
Dickson Anyaele <dickson.anyaele@amalitech.com>
2024-01-02 19:17:09 +0000

    feat(minor): added cross origin to brand controller

--- src/main/java/com/amalitech/gpuconfigurator/controller/BrandController.java
+++ src/main/java/com/amalitech/gpuconfigurator/controller/BrandController.java
@@ -21,24 +21,28 @@ public class BrandController {
 
     private final BrandService brandService;
 
+    @CrossOrigin
     @PostMapping("/v1/admin/brand")
     public ResponseEntity<Brand> createBrand(@RequestBody BrandRequest request) {
         Brand brand = brandService.createBrand(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(brand);
     }
 
+    @CrossOrigin
     @GetMapping("/v1/admin/brand")
     public ResponseEntity<List<BrandDto>> getAllBrands() {
         List<BrandDto> brands = brandService.getAllBrands();
         return ResponseEntity.ok(brands);
     }
 
+    @CrossOrigin
     @DeleteMapping("/v1/admin/brand/{brandId}")
     public ResponseEntity<GenericResponse> deleteBrand(@PathVariable String brandId) {
         GenericResponse deletedBrand = brandService.deleteBrand(UUID.fromString(brandId));
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(deletedBrand);
     }
 
+    @CrossOrigin
     @PutMapping("/v1/admin/brand/{brandId}")
     public ResponseEntity<GenericResponse> updatedBrand(@PathVariable String brandId, @RequestBody BrandRequest request) {
         GenericResponse updatedBrand = brandService.updatedBrand(UUID.fromString(brandId), request);
