128566b1991aea1b1fca73c3b3ebb695b4587c1c
Dickson Anyaele <dickson.anyaele@amalitech.com>
2024-01-02 11:38:03 +0000

    feat: added repository tags to both brand repo and product brand repo and created barebone brand service class

--- src/main/java/com/amalitech/gpuconfigurator/repository/ProductBrandRepository.java
+++ src/main/java/com/amalitech/gpuconfigurator/repository/ProductBrandRepository.java
@@ -2,9 +2,11 @@ package com.amalitech.gpuconfigurator.repository;
 
 import com.amalitech.gpuconfigurator.model.ProductBrand;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ProductBrandRepository extends JpaRepository<ProductBrand, UUID> {
 
     ProductBrand findByProductId(UUID productId);
---
+++ src/main/java/com/amalitech/gpuconfigurator/service/BrandServiceImpl.java
@@ -0,0 +1,4 @@
+package com.amalitech.gpuconfigurator.service;
+
+public class BrandServiceImpl {
+}
