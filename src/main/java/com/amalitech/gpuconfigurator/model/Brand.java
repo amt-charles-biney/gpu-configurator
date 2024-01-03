a609031ce481d2f7dd0c2de29ff39ffbf6315521
Dickson Anyaele <dickson.anyaele@amalitech.com>
2024-01-03 13:15:17 +0000

    feat: added Brand Model

---
+++ src/main/java/com/amalitech/gpuconfigurator/model/Brand.java
@@ -0,0 +1,43 @@
+package com.amalitech.gpuconfigurator.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "brand")
+public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable=false, unique = true)
+    private String name;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deleted_at;
+
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    }
