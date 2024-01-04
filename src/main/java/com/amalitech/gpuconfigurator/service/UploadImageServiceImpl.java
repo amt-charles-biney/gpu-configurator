afdcf200bd3ad5043e18d5d127d080c9f7d0fdb7
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2024-01-04 14:19:35 +0000

    fix: conflicting beans

--- src/main/java/com/amalitech/gpuconfigurator/service/UploadImageServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.amalitech.gpuconfigurator.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface UploadImageServiceImpl {
-     String upload(MultipartFile file);
-    String uploadCoverImage(MultipartFile coverImage);
-
-}
