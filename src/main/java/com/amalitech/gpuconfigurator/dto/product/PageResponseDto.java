e5140a6abaf4f7c6a5eeaded70ed3161748f3ff9
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2024-01-04 12:56:29 +0000

    fix:typos

--- src/main/java/com/amalitech/gpuconfigurator/dto/product/PageResponseDto.java
+++ src/main/java/com/amalitech/gpuconfigurator/dto/product/PageResponseDto.java
@@ -1,14 +1,13 @@
 package com.amalitech.gpuconfigurator.dto.product;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
-@Builder
 @Data
-@RequiredArgsConstructor
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PageResponseDto {
     private List<ProductResponse> products;
     private long total;
