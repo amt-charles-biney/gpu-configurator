e877e3cf471d8df7a0974f061472d1b1e83b8a16
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2024-01-04 09:55:53 +0000

    feat: product model added

---
+++ src/main/java/com/amalitech/gpuconfigurator/model/Product.java
@@ -0,0 +1,63 @@
+package com.amalitech.gpuconfigurator.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "Product")
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+
+    @Column(name = "product_id", nullable = false)
+    private String productId;
+
+    @Column(name = "product_description", nullable = false)
+    private String productDescription;
+
+    @Column(name = "product_price", nullable = false)
+    private Double productPrice;
+
+    @Column(name = "product_instock", nullable = false)
+    private Integer inStock;
+
+
+    @Column(name = "image_url", nullable = false)
+    private List<String> imageUrl;
+
+    @Column(name = "cover_image_url", nullable = false)
+    private String coverImage;
+
+    @Column(name = "product_availability", nullable = false)
+    private Boolean productAvailability;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        productAvailability = true;
+    }
+
+}
+
