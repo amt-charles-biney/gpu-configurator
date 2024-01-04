4bdc47b608caf6ca6688d709138e3552019b8282
Dickson Anyaele <dickson.anyaele@amalitech.com>
2024-01-04 11:36:53 +0000

    feat: added Category config controller

---
+++ src/main/java/com/amalitech/gpuconfigurator/controller/CategoryConfigController.java
@@ -0,0 +1,34 @@
+package com.amalitech.gpuconfigurator.controller;
+
+import com.amalitech.gpuconfigurator.dto.GenericResponse;
+import com.amalitech.gpuconfigurator.dto.categoryconfig.CategoryConfigResponseDto;
+import com.amalitech.gpuconfigurator.dto.categoryconfig.CompatibleOptionDTO;
+import com.amalitech.gpuconfigurator.service.category.CategoryConfig.CategoryConfigService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class CategoryConfigController {
+
+    private final CategoryConfigService categoryConfigService;
+
+    @CrossOrigin
+    @PostMapping("/v1/admin/category/{categoryId}/config")
+    public ResponseEntity<GenericResponse> addConfig(@PathVariable String categoryId, @RequestBody List<CompatibleOptionDTO> categoryConfigDto) {
+        GenericResponse result = categoryConfigService.createCategoryConfig(UUID.fromString(categoryId), categoryConfigDto);
+        return ResponseEntity.ok(result);
+    }
+
+    @CrossOrigin
+    @GetMapping("/v1/admin/category/{categoryId}/config")
+    public ResponseEntity<CategoryConfigResponseDto> getConfigs(@PathVariable String categoryId) {
+        CategoryConfigResponseDto result = categoryConfigService.getCategoryConfigByCategory(categoryId);
+        return ResponseEntity.ok(result);
+    }
+}
