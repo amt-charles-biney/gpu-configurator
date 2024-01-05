22d8977484f2ab4a8c06c7c96e7a96d68ad2cec7
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2024-01-05 10:35:05 +0000

    feat: brandname update

--- src/main/java/com/amalitech/gpuconfigurator/dto/product/ProductUpdateDto.java
+++ src/main/java/com/amalitech/gpuconfigurator/dto/product/ProductUpdateDto.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
 
 @Data
 @Builder
@@ -20,4 +18,5 @@ public class ProductUpdateDto {
     Boolean availability;
     Integer inStock;
     String category;
+    String brand;
 }
