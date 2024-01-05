35614d23509cbdb26ed10cbcd98eeb696d59f3ac
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2024-01-05 12:25:50 +0000

    fix: error details

--- src/main/java/com/amalitech/gpuconfigurator/exception/CustomExceptionHandler.java
+++ src/main/java/com/amalitech/gpuconfigurator/exception/CustomExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.amalitech.gpuconfigurator.exception;
 
 import jakarta.mail.MessagingException;
+import jakarta.persistence.EntityNotFoundException;
 import org.apache.coyote.BadRequestException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,6 +22,26 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleUncheckedExceptions(Exception e) {
+
+        if (e instanceof MaxUploadSizeExceededException ||
+                e instanceof BadRequestException ||
+                e instanceof DataIntegrityViolationException ||
+                e instanceof EntityNotFoundException) {
+            return buildProblemDetail(400, e.getMessage(), e.getMessage());
+        }
+        return buildProblemDetail(500, "Something went wrong", e.getMessage());
+    }
+
+    private ProblemDetail buildProblemDetail(int statusCode, String detail, String message) {
+        ProblemDetail errorDetail = ProblemDetail.forStatus(HttpStatusCode.valueOf(statusCode));
+        errorDetail.setDetail(detail);
+        String setProperty = "Bad Request";
+        errorDetail.setProperty(setProperty, message);
+        return errorDetail;
+    }
     @ExceptionHandler({MessagingException.class})
     public ProblemDetail handleEmailSendingException(MessagingException e) {
         ProblemDetail errorDetail = ProblemDetail.forStatus(HttpStatusCode.valueOf(500));
