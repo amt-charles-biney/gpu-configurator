41fd04ec67c3a24a8e7a27c752cf1a1acb2c2902
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2024-01-06 23:38:56 +0000

    refactor: test cases for category

--- src/test/java/com/amalitech/gpuconfigurator/controller/CategoryControllerTest.java
+++ src/test/java/com/amalitech/gpuconfigurator/controller/CategoryControllerTest.java
@@ -4,14 +4,24 @@ package com.amalitech.gpuconfigurator.controller;
 import com.amalitech.gpuconfigurator.dto.category.AllCategoryResponse;
 import com.amalitech.gpuconfigurator.dto.category.CategoryRequestDto;
 import com.amalitech.gpuconfigurator.model.Category;
+import com.amalitech.gpuconfigurator.repository.CategoryRepository;
 import com.amalitech.gpuconfigurator.service.CategoryService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,52 +32,63 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 
+@SpringBootTest
 @ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc(addFilters = false)
 class CategoryControllerTest {
 
     @InjectMocks
     private CategoryController categoryController;
 
-    @Mock
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
     private CategoryService categoryService;
+
+    @Mock
+    private CategoryRepository categoryRepository;
 
     @Test
     @DisplayName("Test success for creating new category")
-    void createCategory() {
+    void createCategory() throws Exception {
         CategoryRequestDto dto = new CategoryRequestDto("GPU");
+
         Category expectedCategory = Category.builder()
-                .id(UUID.fromString("6c4a87df-6831-4d49-a924-42979ec657ba"))
+                .id(UUID.randomUUID())
                 .categoryName(dto.name())
                 .build();
 
         when(categoryService.createCategory(dto)).thenReturn(expectedCategory);
-        Category response = categoryController.createCategory(dto);
 
-        assertNotNull(response.getId());
-        assertEquals(expectedCategory, response);
-        assertEquals(HttpStatus.CREATED.value(), 201);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/admin/category")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        .content(new ObjectMapper().writeValueAsString(dto)))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
     }
 
     @Test
     @DisplayName("Test success for getting all categories")
-    void getAllCategoriesTest() {
+    void getAllCategoriesTest() throws Exception {
         List<AllCategoryResponse> expectedCategories = Arrays.asList(
                 AllCategoryResponse.builder().categoryName("SERVER").build(),
                 AllCategoryResponse.builder().categoryName("GPU").build()
         );
 
         when(categoryService.getAllCategories()).thenReturn(expectedCategories);
-        List<AllCategoryResponse> response = categoryController.getAllCategories();
 
-
-        assertEquals(expectedCategories.size(), response.size());
-        assertEquals(expectedCategories, response);
-        assertEquals(HttpStatus.OK.value(), 200);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/admin/category"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(expectedCategories.size()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].categoryName").value("SERVER"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].categoryName").value("GPU"));
     }
 
     @Test
     @DisplayName("Test success for getting category by name")
-    void getCategoryByNameTest() {
+    void getCategoryByNameTest() throws Exception {
         String categoryName = "GPU";
         Category expectedCategory = Category.builder()
                 .id(UUID.fromString("6c4a87df-6831-4d49-a924-42979ec657ba"))
@@ -75,11 +96,13 @@ class CategoryControllerTest {
                 .build();
 
         when(categoryService.getCategoryByName(categoryName)).thenReturn(expectedCategory);
-        Category response = categoryController.getCategoryByName(categoryName);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/admin/category/{name}", categoryName))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value("6c4a87df-6831-4d49-a924-42979ec657ba"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.categoryName").value(categoryName));
 
-        assertEquals(expectedCategory.getCategoryName(), response.getCategoryName());
-        assertEquals(expectedCategory, response);
-        assertEquals(HttpStatus.OK.value(), 200);
+
     }
 }
 
