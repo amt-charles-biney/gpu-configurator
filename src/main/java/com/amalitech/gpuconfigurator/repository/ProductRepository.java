342b1d961050983790a68eafdf3aea5ec6e79ec7
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2024-01-06 16:29:20 +0000

    feat: merge product model

---
+++ src/main/java/com/amalitech/gpuconfigurator/repository/ProductRepository.java
@@ -0,0 +1,27 @@
+package com.amalitech.gpuconfigurator.repository;
+
+import com.amalitech.gpuconfigurator.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+
+    @Modifying
+    void deleteById(UUID id);
+
+    @Query("SELECT i FROM Product i WHERE i.productId =?1")
+    Optional<Product> getProductByProductId(String productId);
+
+    @Query("SELECT p FROM Product p WHERE p.featured = true")
+    Optional<List<Product>> getFeaturedProduct();
+
+}
