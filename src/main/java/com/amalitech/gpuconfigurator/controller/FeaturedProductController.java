e7d7ddf27883715e729e4720f601847f8c6b1a30
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2024-01-06 16:20:18 +0000

    feat: featured product controllers added

---
+++ src/main/java/com/amalitech/gpuconfigurator/controller/FeaturedProductController.java
@@ -0,0 +1,35 @@
+package com.amalitech.gpuconfigurator.controller;
+
+import com.amalitech.gpuconfigurator.dto.FeaturedResponseDto;
+import com.amalitech.gpuconfigurator.model.Product;
+import com.amalitech.gpuconfigurator.service.product.FeaturedService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class FeaturedProductController {
+    private final FeaturedService featuredService;
+
+    @CrossOrigin
+    @GetMapping("/v1/featured")
+    public List<Product> getAllFeaturedProduct(){
+        return featuredService.getAllFeaturedProduct();
+    }
+
+    @CrossOrigin
+    @PostMapping("/v1/featured/{id}")
+    public FeaturedResponseDto addFeaturedProduct(@PathVariable("id") UUID id){
+        return featuredService.addFeaturedProduct(id);
+    }
+
+    @CrossOrigin
+    @PutMapping("/v1/featured/{id}")
+    public FeaturedResponseDto removeFeaturedProduct(@PathVariable("id") UUID id){
+        return featuredService.removeFeaturedProduct(id);
+    }
+}
