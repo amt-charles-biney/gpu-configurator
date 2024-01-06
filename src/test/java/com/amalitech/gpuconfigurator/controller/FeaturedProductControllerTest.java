8d1e51affff04a68a9d2c5db5290b3d572d5f1cc
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2024-01-06 21:27:04 +0000

    feat: test for featured product

--- src/test/java/com/amalitech/gpuconfigurator/controller/FeaturedProductControllerTest.java
+++ src/test/java/com/amalitech/gpuconfigurator/controller/FeaturedProductControllerTest.java
@@ -1,5 +1,9 @@
 package com.amalitech.gpuconfigurator.controller;
 
+import com.amalitech.gpuconfigurator.dto.FeaturedResponseDto;
+import com.amalitech.gpuconfigurator.model.Category;
+import com.amalitech.gpuconfigurator.model.Product;
+import com.amalitech.gpuconfigurator.repository.ProductRepository;
 import com.amalitech.gpuconfigurator.service.product.FeaturedService;
 import com.amalitech.gpuconfigurator.service.product.FeaturedServiceImpl;
 import org.junit.jupiter.api.Test;
@@ -8,14 +12,23 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
 @ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc(addFilters = false)
 class FeaturedProductControllerTest {
 
     @InjectMocks
@@ -27,18 +40,70 @@ class FeaturedProductControllerTest {
     @MockBean
     private FeaturedServiceImpl featuredService;
 
+    @Mock
+    private ProductRepository productRepository;
+
 
     @Test
     void getAllFeaturedProduct() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/admin/brand"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/admin/feature"))
+                .andExpect(status().isOk());
     }
 
     @Test
-    void addFeaturedProduct() {
+    void addFeaturedProduct() throws Exception {
+        UUID productId = UUID.randomUUID();
+        Category category = Category.builder().categoryName("GPU").build();
+        Product product = Product.builder()
+                .id(productId)
+                .productName("Test")
+                .productDescription("powerful gpu")
+                .productPrice(20.00)
+                .productAvailability(true)
+                .inStock(10)
+                .category(category)
+                .createdAt(LocalDateTime.now())
+                .productId("abcd")
+                .build();
+
+        when(featuredService.addFeaturedProduct(productId)).thenReturn(
+                FeaturedResponseDto.builder().message("Now a featured Product").build());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/admin/featured/{id}", productId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8"))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Now a featured Product"))
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+
     }
 
     @Test
-    void removeFeaturedProduct() {
+    void removeFeaturedProduct() throws Exception {
+        UUID productId = UUID.randomUUID();
+        Category category = Category.builder().categoryName("GPU").build();
+        Product product = Product.builder()
+                .id(productId)
+                .productName("Test")
+                .productDescription("powerful gpu")
+                .productPrice(20.00)
+                .productAvailability(true)
+                .inStock(10)
+                .featured(true)
+                .category(category)
+                .createdAt(LocalDateTime.now())
+                .productId("abcd")
+                .build();
+
+        when(featuredService.removeFeaturedProduct(productId)).thenReturn(
+                FeaturedResponseDto.builder().message("Product is no more featured").build());
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/admin/featured/{id}", productId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Product is no more featured"))
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+
     }
 }
