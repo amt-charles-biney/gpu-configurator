86e86159ab82e178bc8d027079d304698a43f152
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-01-14 07:34:09 -1200

    refactor: changed media and unit in attribute option response dto to optionMedia and optionUnit

--- src/main/java/com/amalitech/gpuconfigurator/dto/attribute/AttributeOptionResponseDto.java
+++ src/main/java/com/amalitech/gpuconfigurator/dto/attribute/AttributeOptionResponseDto.java
@@ -5,5 +5,5 @@ import lombok.Builder;
 import java.math.BigDecimal;
 
 @Builder
-public record AttributeOptionResponseDto(String id, String optionName, BigDecimal optionPrice, String media, Integer unit, AttributeResponseDto attribute) {
+public record AttributeOptionResponseDto(String id, String optionName, BigDecimal optionPrice, String optionMedia, Integer optionUnit, AttributeResponseDto attribute) {
 }
