4b9332397ca1bd5836b326d22b1d0082d834886e
Dickson Anyaele <dickson.anyaele@amalitech.com>
2024-01-16 08:46:14 +0000

    refactor: converted post and put attribute options from json to formdata

--- src/main/java/com/amalitech/gpuconfigurator/dto/attribute/AttributeOptionDto.java
+++ src/main/java/com/amalitech/gpuconfigurator/dto/attribute/AttributeOptionDto.java
@@ -1,12 +1,14 @@
 package com.amalitech.gpuconfigurator.dto.attribute;
 
+import jakarta.mail.Multipart;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
 
 public record AttributeOptionDto(
         @NotNull(message = "attribute option cannot be null")
                 @NotBlank(message = "attribute option cannot blank")
-        String name, BigDecimal price, String media, String unit, Float baseAmount, Float maxAmount, Float priceIncrement) {
+        String name, BigDecimal price, MultipartFile media, String unit, Float baseAmount, Float maxAmount, Float priceIncrement) {
 }
