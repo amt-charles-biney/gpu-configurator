f3d7b67a184ecb1fb3fea50306f09f674086655f
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2024-01-18 10:00:24 +0000

    fix: saving with query

--- src/main/java/com/amalitech/gpuconfigurator/controller/ConfigurationController.java
+++ src/main/java/com/amalitech/gpuconfigurator/controller/ConfigurationController.java
@@ -1,6 +1,5 @@
 package com.amalitech.gpuconfigurator.controller;
 
-import com.amalitech.gpuconfigurator.dto.configuration.ConfigurationRequestDto;
 import com.amalitech.gpuconfigurator.dto.configuration.ConfigurationResponseDto;
 import com.amalitech.gpuconfigurator.service.configuration.ConfigurationService;
 import lombok.RequiredArgsConstructor;
@@ -15,24 +14,16 @@ public class ConfigurationController {
 
     private final ConfigurationService configurationService;
 
-
-    @CrossOrigin
-    @PostMapping("/v1/configuration")
-    @ResponseStatus(HttpStatus.CREATED)
-    public ConfigurationResponseDto createConfiguration(@RequestBody ConfigurationRequestDto request) {
-        return configurationService.createConfiguration(request);
-    }
-
-
     @CrossOrigin
     @GetMapping("/v1/config/{productId}")
     @ResponseStatus(HttpStatus.OK)
     public ConfigurationResponseDto configuration(
             @PathVariable("productId") String productId,
             @RequestParam(required = false) String components,
-           @RequestParam(required = false) Boolean warranty
-            ) {
-        return configurationService.configuration(productId, components,warranty);
+            @RequestParam(required = false) Boolean warranty,
+            @RequestParam(required = false) Boolean save
+    ) {
+        return configurationService.configuration(productId, components, warranty, save);
     }
 
 }
--- src/main/java/com/amalitech/gpuconfigurator/service/configuration/ConfigurationServiceImpl.java
+++ src/main/java/com/amalitech/gpuconfigurator/service/configuration/ConfigurationServiceImpl.java
@@ -1,6 +1,5 @@
 package com.amalitech.gpuconfigurator.service.configuration;
 
-import com.amalitech.gpuconfigurator.dto.configuration.ConfigurationRequestDto;
 import com.amalitech.gpuconfigurator.dto.configuration.ConfigurationResponseDto;
 import com.amalitech.gpuconfigurator.model.CategoryConfig;
 import com.amalitech.gpuconfigurator.model.CompatibleOption;
@@ -10,7 +9,6 @@ import com.amalitech.gpuconfigurator.model.configuration.Configuration;
 import com.amalitech.gpuconfigurator.repository.CategoryConfigRepository;
 import com.amalitech.gpuconfigurator.repository.ConfigurationRepository;
 import com.amalitech.gpuconfigurator.repository.ProductRepository;
-import com.amalitech.gpuconfigurator.service.category.CategoryConfig.CategoryConfigService;
 import com.amalitech.gpuconfigurator.service.category.compatible.CompatibleOptionService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -26,57 +24,12 @@ import java.util.UUID;
 public class ConfigurationServiceImpl implements ConfigurationService {
     private final CompatibleOptionService compatibleOptionService;
     private final ProductRepository productRepository;
-    private final CategoryConfigService categoryConfigService;
     private final CategoryConfigRepository categoryConfigRepository;
     private final ConfigurationRepository configurationRepository;
 
 
     @Override
-    public ConfigurationResponseDto createConfiguration(ConfigurationRequestDto request) {
-        Product product = productRepository.findById(UUID.fromString(request.productId()))
-                .orElseThrow(() -> new EntityNotFoundException("Product not found"));
-
-        BigDecimal totalPrice = BigDecimal.valueOf(product.getProductPrice());
-
-
-        CategoryConfig categoryConfig = categoryConfigRepository.findByCategoryId(UUID.fromString(request.categoryId()))
-                .orElseThrow(() -> new EntityNotFoundException("Configuration does not exist for the specified category"));
-
-        List<CompatibleOption> compatibleOptions = compatibleOptionService.getByCategoryConfigId(categoryConfig.getId());
-
-        List<ConfigOptions> configOptions = compatibleOptions.stream()
-                .filter(CompatibleOption::getIsIncluded)
-                .map(option -> ConfigOptions.builder()
-                        .optionId(String.valueOf(option.getId()))
-                        .optionName(option.getName())
-                        .optionPrice(option.getPrice())
-                        .optionType(option.getType())
-                        .build())
-                .toList();
-
-        BigDecimal optionalTotal = configOptions.stream()
-                .map(ConfigOptions::getOptionPrice)
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
-        totalPrice = totalPrice.add(optionalTotal);
-
-        Configuration configuration = Configuration.builder()
-                .totalPrice(totalPrice)
-                .product(product)
-                .configuredOptions(configOptions)
-                .build();
-        configurationRepository.save(configuration);
-
-        return ConfigurationResponseDto.builder()
-                .productId(String.valueOf(configuration.getProduct().getId()))
-                .totalPrice(totalPrice)
-                .configuredPrice(optionalTotal)
-                .productPrice(BigDecimal.valueOf(product.getProductPrice()))
-                .configured(configOptions)
-                .build();
-    }
-
-    @Override
-    public ConfigurationResponseDto configuration(String productId, String components, Boolean warranty) {
+    public ConfigurationResponseDto configuration(String productId, String components, Boolean warranty, Boolean save) {
         String[] componentIds = components != null ? components.split(",") : new String[0];
 
         Product product = productRepository.findById(UUID.fromString(productId))
@@ -127,6 +80,10 @@ public class ConfigurationServiceImpl implements ConfigurationService {
                 .product(product)
                 .configuredOptions(configOptions)
                 .build();
+
+        if(save != null && save){
+            configurationRepository.save(configuration);
+        }
 
         return ConfigurationResponseDto.builder()
                 .productId(String.valueOf(configuration.getProduct().getId()))
