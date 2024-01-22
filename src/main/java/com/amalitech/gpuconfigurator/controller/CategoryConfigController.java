c2651c25649714e86cb5fb2445fc2f981504f4ea
Dickson Anyaele <dickson.anyaele@amalitech.com>
2024-01-22 13:46:33 +0000

    ?

--- src/main/java/com/amalitech/gpuconfigurator/controller/CategoryConfigController.java
+++ src/main/java/com/amalitech/gpuconfigurator/controller/CategoryConfigController.java
@@ -1,9 +1,10 @@
 package com.amalitech.gpuconfigurator.controller;
 
-import com.amalitech.gpuconfigurator.dto.GenericResponse;
+import com.amalitech.gpuconfigurator.dto.categoryconfig.CategoryConfigRequest;
 import com.amalitech.gpuconfigurator.dto.categoryconfig.CategoryConfigResponseDto;
 import com.amalitech.gpuconfigurator.dto.categoryconfig.CompatibleOptionDTO;
-import com.amalitech.gpuconfigurator.service.categoryConfig.CategoryConfigService;
+import com.amalitech.gpuconfigurator.dto.GenericResponse;
+import com.amalitech.gpuconfigurator.service.category.CategoryConfig.CategoryConfigService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,15 +20,22 @@ public class CategoryConfigController {
     private final CategoryConfigService categoryConfigService;
 
     @CrossOrigin
-    @PostMapping("/v1/admin/category/{categoryId}/config")
-    public ResponseEntity<GenericResponse> addConfig(@PathVariable String categoryId, @RequestBody List<CompatibleOptionDTO> categoryConfigDto) {
-        GenericResponse result = categoryConfigService.createCategoryConfig(UUID.fromString(categoryId), categoryConfigDto);
+    @PostMapping("/v1/admin/category/config")
+    public ResponseEntity<GenericResponse> addConfig(@RequestBody CategoryConfigRequest categoryConfigDto) {
+        GenericResponse result = categoryConfigService.createCategoryConfig(categoryConfigDto);
         return ResponseEntity.ok(result);
     }
 
     @CrossOrigin
     @GetMapping("/v1/admin/category/{categoryId}/config")
     public ResponseEntity<CategoryConfigResponseDto> getConfigs(@PathVariable String categoryId) {
+        CategoryConfigResponseDto result = categoryConfigService.getCategoryConfigByCategory(categoryId);
+        return ResponseEntity.ok(result);
+    }
+
+    @CrossOrigin
+    @GetMapping("/v1/category/{categoryId}/config")
+    public ResponseEntity<CategoryConfigResponseDto> getConfigsUser(@PathVariable String categoryId) {
         CategoryConfigResponseDto result = categoryConfigService.getCategoryConfigByCategory(categoryId);
         return ResponseEntity.ok(result);
     }
