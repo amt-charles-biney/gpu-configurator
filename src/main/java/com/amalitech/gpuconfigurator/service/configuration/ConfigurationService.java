9e5bb863a14c9223d7ea222470e6d14a45020bc7
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2024-01-23 09:49:45 +0000

    feat: other CRUD for configuration

--- src/main/java/com/amalitech/gpuconfigurator/service/configuration/ConfigurationService.java
+++ src/main/java/com/amalitech/gpuconfigurator/service/configuration/ConfigurationService.java
@@ -1,6 +1,7 @@
 package com.amalitech.gpuconfigurator.service.configuration;
 
 import com.amalitech.gpuconfigurator.dto.configuration.ConfigurationResponseDto;
+import com.amalitech.gpuconfigurator.model.configuration.Configuration;
 
 
 public interface ConfigurationService {
@@ -8,4 +9,8 @@ public interface ConfigurationService {
     ConfigurationResponseDto configuration(String productId, Boolean warranty, Boolean save, String components);
 
     ConfigurationResponseDto saveConfiguration(String productId, Boolean warranty, Boolean save, String components);
+
+    Configuration getSpecificConfiguration(String configId);
+
+    void deleteSpecificConfiguration(String configId);
 }
