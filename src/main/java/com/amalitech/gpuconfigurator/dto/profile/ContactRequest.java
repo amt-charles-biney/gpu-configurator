a3634a75889e3e866442446bfd97a0df5c149d64
Joseph Ampadu <joseph.ampadu@amalitech.com>
2024-01-24 10:23:13 +0000

    test: add tests for ContactService

--- src/main/java/com/amalitech/gpuconfigurator/dto/profile/ContactRequest.java
+++ src/main/java/com/amalitech/gpuconfigurator/dto/profile/ContactRequest.java
@@ -1,8 +1,14 @@
 package com.amalitech.gpuconfigurator.dto.profile;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class ContactRequest {
     @NotBlank
