38a207c4cf5ecceac67daccc04e29d0514969f1c
Dickson Anyaele <dickson.anyaele@amalitech.com>
2024-01-29 14:48:08 +0000

    refactor: updated the dtos for compatible options

--- src/main/java/com/amalitech/gpuconfigurator/dto/categoryconfig/CompatibleOptionDTO.java
+++ src/main/java/com/amalitech/gpuconfigurator/dto/categoryconfig/CompatibleOptionDTO.java
@@ -1,11 +1,13 @@
 package com.amalitech.gpuconfigurator.dto.categoryconfig;
 
 import com.amalitech.gpuconfigurator.model.CategoryConfig;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.math.BigDecimal;
 
 @Builder
-public record CompatibleOptionDTO(String name, String type, CategoryConfig categoryConfig, BigDecimal price,
-                                  String media, String unit, Boolean isCompatible, Boolean isIncluded, Double priceFactor, Boolean isMeasured, Float baseAmount, Float maxAmount, Float priceIncrement) {
+public record CompatibleOptionDTO(CategoryConfig categoryConfig,
+                                  @NotNull(message = "attribute option id must exixt")
+                                  String attributeOptionId, Boolean isCompatible, Boolean isIncluded, boolean isMeasured, Integer size, String attributeId) {
 }
