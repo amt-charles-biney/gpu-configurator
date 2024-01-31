aaf48a030d988681bb0aa7d4cdfc01a69777ed3f
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2024-01-31 11:41:07 +0000

    fix: custom exception handler

--- src/main/java/com/amalitech/gpuconfigurator/exception/CustomExceptionHandler.java
+++ src/main/java/com/amalitech/gpuconfigurator/exception/CustomExceptionHandler.java
@@ -3,9 +3,7 @@ package com.amalitech.gpuconfigurator.exception;
 import jakarta.mail.MessagingException;
 import jakarta.persistence.EntityNotFoundException;
 import org.apache.coyote.BadRequestException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ProblemDetail;
+import org.springframework.http.*;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.FieldError;
@@ -13,61 +11,72 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
 
+
 @RestControllerAdvice
-public class CustomExceptionHandler {
-    @ExceptionHandler({ MessagingException.class })
-    public ProblemDetail handleEmailSendingException(MessagingException e) {
-        ProblemDetail errorDetail = ProblemDetail.forStatus(HttpStatusCode.valueOf(500));
-        errorDetail.setDetail(e.getMessage());
-        return errorDetail;
+
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGenericException(Exception e) {
+        return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
     }
 
-    @ExceptionHandler({ MethodArgumentNotValidException.class, BadRequestException.class })
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ProblemDetail handleValidationExceptions(MethodArgumentNotValidException ex) {
-        ProblemDetail errorDetail = ProblemDetail.forStatus(HttpStatusCode.valueOf(400));
-        errorDetail.setDetail("One or more validation errors occurred");
+    @ExceptionHandler({MessagingException.class})
+    public ResponseEntity<Object> handleEmailSendingException(MessagingException e) {
+        return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+    }
 
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
         Map<String, Object> fieldErrors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach(error -> {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
             fieldErrors.put(fieldName, errorMessage);
         });
+
+        ProblemDetail errorDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        errorDetail.setDetail("One or more validation errors occurred");
         errorDetail.setProperty("field_errors", fieldErrors);
-        return errorDetail;
+
+        return ResponseEntity.badRequest().body(errorDetail);
     }
 
-    @ExceptionHandler({ BadCredentialsException.class })
-    public ProblemDetail handleBadCredentialException(Exception e) {
-        ProblemDetail errorDetail = ProblemDetail.forStatus(HttpStatusCode.valueOf(401));
-        errorDetail.setDetail(e.getMessage());
-        return errorDetail;
+    @ExceptionHandler({BadCredentialsException.class})
+    public ResponseEntity<Object> handleBadCredentialException(Exception e) {
+        return buildResponse(HttpStatus.UNAUTHORIZED, e.getMessage());
     }
 
-    @ExceptionHandler({ UsernameNotFoundException.class, NotFoundException.class, EntityNotFoundException.class })
-    public ProblemDetail usernameNotFoundException(Exception e) {
-        ProblemDetail errorDetail = ProblemDetail.forStatus(HttpStatusCode.valueOf(404));
-        errorDetail.setDetail(e.getMessage());
-        return errorDetail;
+
+    @ExceptionHandler({UsernameNotFoundException.class, NotFoundException.class, EntityNotFoundException.class})
+    public ResponseEntity<Object> handleNotFoundException(Exception e) {
+        return buildResponse(HttpStatus.NOT_FOUND, e.getMessage());
     }
 
     @ExceptionHandler(AttributeNameAlreadyExistsException.class)
-    public ProblemDetail AttributeNameAlreadyExistsException(Exception e) {
-        ProblemDetail errorDetail = ProblemDetail.forStatus(HttpStatusCode.valueOf(400));
-        errorDetail.setDetail(e.getMessage());
-        return errorDetail;
+    public ResponseEntity<Object> handleAttributeNameAlreadyExistsException(Exception e) {
+        return buildResponse(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
     @ExceptionHandler(InvalidPasswordException.class)
-    public ProblemDetail handleInvalidPasswordException(Exception e) {
-        ProblemDetail errorDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
-        errorDetail.setDetail(e.getMessage());
-        return errorDetail;
+    public ResponseEntity<Object> handleInvalidPasswordException(Exception e) {
+        return buildResponse(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
+    private ResponseEntity<Object> buildResponse(HttpStatus status, String detail) {
+        ProblemDetail errorDetail = ProblemDetail.forStatus(status);
+        errorDetail.setDetail(detail);
+        return ResponseEntity.status(status).body(errorDetail);
+    }
 }
