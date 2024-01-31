e50e07f90dd7c54472b9279e16dfd8bb05150c49
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2024-01-31 07:49:10 +0000

    fix 2

--- src/main/java/com/amalitech/gpuconfigurator/dto/categoryconfig/CompatibleOptionResponseDto.java
+++ src/main/java/com/amalitech/gpuconfigurator/dto/categoryconfig/CompatibleOptionResponseDto.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import java.math.BigDecimal;
 
 @Builder
-public record CompatibleOptionResponseDto(String id , String name, String type, BigDecimal price,
-                                  String media, String unit, Boolean isCompatible, Boolean isIncluded, Boolean isMeasured, Double priceFactor, Integer size, String attributeId, String attributeOptionId,  Float priceIncrement, Float baseAmount, Float maxAmount) {
+public record CompatibleOptionResponseDto(String compatibleOptionId, String name, String type, BigDecimal price,
+                                          String media, String unit, Boolean isCompatible, Boolean isIncluded,
+                                          Boolean isMeasured, Double priceFactor, Integer size, String attributeId,
+                                          String attributeOptionId, Float priceIncrement, Float baseAmount,
+                                          Float maxAmount) {
 }
