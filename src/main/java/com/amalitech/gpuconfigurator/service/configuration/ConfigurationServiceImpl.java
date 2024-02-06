858c49cce4636be7ebefb6a262d35bf59ce0b58d
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2024-02-06 11:32:49 +0000

    fix: configuration option price

--- src/main/java/com/amalitech/gpuconfigurator/service/configuration/ConfigurationServiceImpl.java
+++ src/main/java/com/amalitech/gpuconfigurator/service/configuration/ConfigurationServiceImpl.java
@@ -143,7 +143,7 @@ public class ConfigurationServiceImpl implements ConfigurationService {
             return ConfigOptions.builder()
                     .optionId(String.valueOf(option.getId()))
                     .optionName(option.getAttributeOption().getOptionName())
-                    .optionPrice(calculatedPrice)
+                    .optionPrice(option.getIsIncluded() ? calculatedPrice : BigDecimal.valueOf(option.getAttributeOption().getBaseAmount()).subtract(calculatedPrice))
                     .optionType(option.getAttributeOption().getAttribute().getAttributeName())
                     .baseAmount(BigDecimal.valueOf(option.getAttributeOption().getBaseAmount()))
                     .isIncluded(option.getIsIncluded())
