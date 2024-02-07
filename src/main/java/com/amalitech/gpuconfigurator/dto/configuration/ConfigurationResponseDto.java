50c25ef7c2c2251e6a426a3b8c499efc2311c50d
Joseph Ampadu <joseph.ampadu@amalitech.com>
2024-02-07 09:54:24 +0000

    fix: change json property for dto field

--- src/main/java/com/amalitech/gpuconfigurator/dto/configuration/ConfigurationResponseDto.java
+++ src/main/java/com/amalitech/gpuconfigurator/dto/configuration/ConfigurationResponseDto.java
@@ -1,13 +1,16 @@
 package com.amalitech.gpuconfigurator.dto.configuration;
 
 import com.amalitech.gpuconfigurator.model.configuration.ConfigOptions;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonKey;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @Builder
-public record ConfigurationResponseDto(String Id,BigDecimal totalPrice, String productId,String productName , BigDecimal productPrice,
+public record ConfigurationResponseDto(@JsonProperty("id") String Id, BigDecimal totalPrice, String productId,String productName , BigDecimal productPrice,
                                        BigDecimal configuredPrice, List<ConfigOptions> configured, BigDecimal vat,
                                        Boolean warranty) {
 }
