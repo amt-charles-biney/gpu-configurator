b0ff6f5fabdd63a53a869afab75e08b61621b971
Joseph Ampadu <joseph.ampadu@amalitech.com>
2024-02-14 10:52:08 +0000

    feat: unit tests for CaseService.createCase

--- src/main/java/com/amalitech/gpuconfigurator/dto/cases/CreateCaseRequest.java
+++ src/main/java/com/amalitech/gpuconfigurator/dto/cases/CreateCaseRequest.java
@@ -1,7 +1,10 @@
 package com.amalitech.gpuconfigurator.dto.cases;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -9,6 +12,9 @@ import java.util.List;
 import java.util.UUID;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateCaseRequest {
     @NotBlank
     private String name;
