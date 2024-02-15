9f72048007b8426f23cd8d3f4ba04a88989a53d0
Joseph Ampadu <joseph.ampadu@amalitech.com>
2024-02-15 11:59:26 +0000

    test: unit tests for CaseService.findAll

--- src/test/java/com/amalitech/gpuconfigurator/service/cases/CaseServiceTests.java
+++ src/test/java/com/amalitech/gpuconfigurator/service/cases/CaseServiceTests.java
@@ -15,6 +15,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -151,5 +154,21 @@ public class CaseServiceTests {
         verify(caseRepository, times(1)).findById(invalidId);
 
         Assertions.assertThat(exception.getMessage()).isNotEmpty();
+    }
+
+    @Test
+    public void findAll_whenValidPageAndSize_returnsCaseResponsePage() {
+        List<Case> productCases = List.of(productCase);
+        Page<Case> casePage = new PageImpl<>(productCases);
+        int page = 0;
+        int size = 2;
+
+        when(caseRepository.findAll(any(Pageable.class))).thenReturn(casePage);
+
+        Page<CaseResponse> result = caseService.findAll(page, size);
+
+        Assertions.assertThat(result).isNotNull();
+        Assertions.assertThat(result.getTotalElements()).isEqualTo(productCases.size());
+        Assertions.assertThat(result.getContent().size()).isEqualTo(productCases.size());
     }
 }
