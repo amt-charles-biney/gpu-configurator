498947574d2abef2c4e55815e5d44e53a181131d
Joseph Ampadu <joseph.ampadu@amalitech.com>
2024-02-15 09:19:45 +0000

    feat: admin get paginated list of cases

--- src/main/java/com/amalitech/gpuconfigurator/controller/CaseController.java
+++ src/main/java/com/amalitech/gpuconfigurator/controller/CaseController.java
@@ -5,6 +5,7 @@ import com.amalitech.gpuconfigurator.dto.cases.CreateCaseRequest;
 import com.amalitech.gpuconfigurator.service.cases.CaseService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +29,15 @@ public class CaseController {
     ) {
         var response = caseService.createCase(dto, coverImage, images);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @CrossOrigin
+    @GetMapping("/v1/admin/cases")
+    public ResponseEntity<Page<CaseResponse>> findAll(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "100") int size
+    ) {
+        return ResponseEntity.ok(caseService.findAll(page, size));
     }
 
     @CrossOrigin
--- src/main/java/com/amalitech/gpuconfigurator/service/cases/CaseService.java
+++ src/main/java/com/amalitech/gpuconfigurator/service/cases/CaseService.java
@@ -2,7 +2,7 @@ package com.amalitech.gpuconfigurator.service.cases;
 
 import com.amalitech.gpuconfigurator.dto.cases.CaseResponse;
 import com.amalitech.gpuconfigurator.dto.cases.CreateCaseRequest;
-import com.amalitech.gpuconfigurator.model.Case;
+import org.springframework.data.domain.Page;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -12,4 +12,6 @@ public interface CaseService {
     CaseResponse createCase(CreateCaseRequest dto, MultipartFile coverImage, List<MultipartFile> images);
 
     CaseResponse updateCase(UUID caseId, CreateCaseRequest dto, MultipartFile coverImage, List<MultipartFile> images);
+
+    Page<CaseResponse> findAll(int page, int size);
 }
