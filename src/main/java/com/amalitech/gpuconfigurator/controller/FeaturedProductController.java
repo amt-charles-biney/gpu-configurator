0d8d2e7009023f8b238dcdfacfcad7592aee6abf
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2024-02-16 11:53:31 +0000

    feat: added a service

---
+++ src/main/java/com/amalitech/gpuconfigurator/controller/FeaturedProductController.java
@@ -0,0 +1,38 @@
+package com.amalitech.gpuconfigurator.controller;
+
+
+import com.amalitech.gpuconfigurator.service.product.FeaturedService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class FeaturedProductController {
+    private final FeaturedService featuredService;
+
+    @CrossOrigin
+    @GetMapping("/v1/featured")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Product> getAllFeaturedProduct(){
+        return featuredService.getAllFeaturedProduct();
+    }
+
+    @CrossOrigin
+    @PostMapping("/v1/admin/featured/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public FeaturedResponseDto addFeaturedProduct(@PathVariable("id") UUID id){
+        return featuredService.addFeaturedProduct(id);
+    }
+
+    @CrossOrigin
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/v1/admin/featured/{id}")
+    public FeaturedResponseDto removeFeaturedProduct(@PathVariable("id") UUID id){
+        return featuredService.removeFeaturedProduct(id);
+    }
+}
