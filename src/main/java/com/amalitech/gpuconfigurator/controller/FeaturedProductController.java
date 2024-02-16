dbaabc6d88a8350ccf8b501829b7392906b6aee0
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2024-02-16 11:55:25 +0000

    feat: added a response dto

--- src/main/java/com/amalitech/gpuconfigurator/controller/FeaturedProductController.java
+++ src/main/java/com/amalitech/gpuconfigurator/controller/FeaturedProductController.java
@@ -1,6 +1,7 @@
 package com.amalitech.gpuconfigurator.controller;
 
 
+import com.amalitech.gpuconfigurator.dto.FeaturedResponseDto;
 import com.amalitech.gpuconfigurator.service.product.FeaturedService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
