9e4c6968f857761d6d0d12cb98343db4408932be
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2024-02-19 15:27:30 +0000

    clean up

--- src/main/java/com/amalitech/gpuconfigurator/model/CategoryConfig.java
+++ src/main/java/com/amalitech/gpuconfigurator/model/CategoryConfig.java
@@ -11,7 +11,6 @@ import java.util.UUID;
 @Getter
 @Setter
 @ToString
-@RequiredArgsConstructor
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
--- src/main/java/com/amalitech/gpuconfigurator/model/attributes/AttributeOption.java
+++ src/main/java/com/amalitech/gpuconfigurator/model/attributes/AttributeOption.java
@@ -3,22 +3,22 @@ package com.amalitech.gpuconfigurator.model.attributes;
 
 import com.amalitech.gpuconfigurator.model.CompatibleOption;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
+import java.util.Objects;
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
+@ToString
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -58,6 +58,7 @@ public class AttributeOption {
     private Integer inStock;
 
     @OneToMany(mappedBy = "attributeOption", cascade = CascadeType.ALL)
+    @ToString.Exclude
     private List<CompatibleOption> compatibleOptions;
 
     @Column(name = "incompatible_attribute_option")
@@ -70,4 +71,20 @@ public class AttributeOption {
     private LocalDateTime updatedAt;
 
     private LocalDateTime deletedAt;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        AttributeOption that = (AttributeOption) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
