2dbd28c7d9a802744fdb795febe4e8dba4b1726d
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2024-02-19 13:31:46 +0000

    refactor: remove @data and added the getters and setters

--- src/main/java/com/amalitech/gpuconfigurator/model/Category.java
+++ src/main/java/com/amalitech/gpuconfigurator/model/Category.java
@@ -1,17 +1,19 @@
 package com.amalitech.gpuconfigurator.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -33,9 +35,8 @@ public class Category {
 
     @OneToMany(
             mappedBy = "category"
-//            cascade = CascadeType.REMOVE
-//            orphanRemoval = false
     )
+    @ToString.Exclude
     private List<Product> products = new ArrayList<>();
 
     @OneToOne(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
@@ -47,4 +48,19 @@ public class Category {
         createdAt = LocalDateTime.now();
     }
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Category category = (Category) o;
+        return getId() != null && Objects.equals(getId(), category.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
