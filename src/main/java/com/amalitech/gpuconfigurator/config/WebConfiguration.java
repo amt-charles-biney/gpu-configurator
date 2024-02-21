c0f87b96fb0216677c53468079bc15f2a58cc5b8
Joseph Ampadu <joseph.ampadu@amalitech.com>
2024-02-21 16:03:29 +0000

    feat: set userSessionId request attribute on every request

--- src/main/java/com/amalitech/gpuconfigurator/config/WebConfiguration.java
+++ src/main/java/com/amalitech/gpuconfigurator/config/WebConfiguration.java
@@ -2,6 +2,7 @@ package com.amalitech.gpuconfigurator.config;
 
 import com.amalitech.gpuconfigurator.interceptors.UserSessionInterceptor;
 import com.amalitech.gpuconfigurator.repository.UserSessionRepository;
+import com.amalitech.gpuconfigurator.service.jwt.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -12,8 +13,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfiguration implements WebMvcConfigurer {
     private final UserSessionRepository userSessionRepository;
 
+    private final JwtService jwtService;
+
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(new UserSessionInterceptor(userSessionRepository));
+        registry.addInterceptor(new UserSessionInterceptor(userSessionRepository, jwtService))
+                .excludePathPatterns("/swagger-ui/**", "/v3/api-docs/**");
     }
 }
