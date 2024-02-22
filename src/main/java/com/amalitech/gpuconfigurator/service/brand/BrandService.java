ac39a42c30b1fee31e0941ed9e85b739082ca04a
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2024-02-22 14:31:42 +0000

    feat: added controller and service

--- src/main/java/com/amalitech/gpuconfigurator/service/brand/BrandService.java
+++ src/main/java/com/amalitech/gpuconfigurator/service/brand/BrandService.java
@@ -1,21 +1,10 @@
 package com.amalitech.gpuconfigurator.service.brand;
 
-import com.amalitech.gpuconfigurator.dto.GenericResponse;
 import com.amalitech.gpuconfigurator.dto.brand.BrandDto;
-import com.amalitech.gpuconfigurator.dto.brand.BrandRequest;
-import com.amalitech.gpuconfigurator.model.Brand;
-import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
-import java.util.UUID;
 
 public interface BrandService {
-
-    Brand createBrand(BrandRequest request);
-
     List<BrandDto> getAllBrands();
 
-    GenericResponse deleteBrand(UUID id);
-
-    GenericResponse updatedBrand(UUID id, @NotNull BrandRequest request);
 }
