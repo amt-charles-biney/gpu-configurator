96d78d69b96522c5346490f9564b1ce9f62b8fb8
Joseph Ampadu <joseph.ampadu@amalitech.com>
2024-02-22 12:31:30 +0000

    feat: update guest user cart management

--- src/main/java/com/amalitech/gpuconfigurator/controller/CartController.java
+++ src/main/java/com/amalitech/gpuconfigurator/controller/CartController.java
@@ -4,8 +4,8 @@ import com.amalitech.gpuconfigurator.dto.cart.AddCartItemResponse;
 import com.amalitech.gpuconfigurator.dto.cart.CartItemsCountResponse;
 import com.amalitech.gpuconfigurator.dto.cart.CartItemsResponse;
 import com.amalitech.gpuconfigurator.dto.cart.DeleteCartItemResponse;
+import com.amalitech.gpuconfigurator.model.UserSession;
 import com.amalitech.gpuconfigurator.service.cart.CartService;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,8 +21,11 @@ public class CartController {
 
     @CrossOrigin
     @GetMapping("/v1/carts/item-count")
-    public ResponseEntity<CartItemsCountResponse> getCartItemsCount(Principal principal, HttpSession session) {
-        return ResponseEntity.ok(cartService.getCartItemsCount(principal, session));
+    public ResponseEntity<CartItemsCountResponse> getCartItemsCount(
+            @RequestAttribute("userSession") UserSession userSession,
+            Principal principal
+    ) {
+        return ResponseEntity.ok(cartService.getCartItemsCount(principal, userSession));
     }
 
     @CrossOrigin
@@ -31,21 +34,28 @@ public class CartController {
             @PathVariable UUID productId,
             @RequestParam(required = false) Boolean warranty,
             @RequestParam(required = false) String components,
-            Principal principal,
-            HttpSession session
+            @RequestAttribute("userSession") UserSession userSession,
+            Principal principal
     ) {
-        return ResponseEntity.ok(cartService.addCartItem(productId, warranty, components, principal, session));
+        return ResponseEntity.ok(cartService.addCartItem(productId, warranty, components, principal, userSession));
     }
 
     @CrossOrigin
     @DeleteMapping("/v1/carts/delete-item/{configuredProductId}")
-    public ResponseEntity<DeleteCartItemResponse> deleteCartItem(@PathVariable UUID configuredProductId, Principal principal, HttpSession session) {
-        return ResponseEntity.ok(cartService.deleteCartItem(configuredProductId, principal, session));
+    public ResponseEntity<DeleteCartItemResponse> deleteCartItem(
+            @PathVariable UUID configuredProductId,
+            @RequestAttribute("userSession") UserSession userSession,
+            Principal principal
+    ) {
+        return ResponseEntity.ok(cartService.deleteCartItem(configuredProductId, principal, userSession));
     }
 
     @CrossOrigin
     @GetMapping("/v1/carts")
-    public ResponseEntity<CartItemsResponse> getCartItems(Principal principal, HttpSession session) {
-        return ResponseEntity.ok(cartService.getCartItems(principal, session));
+    public ResponseEntity<CartItemsResponse> getCartItems(
+            @RequestAttribute("userSession") UserSession userSession,
+            Principal principal
+    ) {
+        return ResponseEntity.ok(cartService.getCartItems(principal, userSession));
     }
 }
