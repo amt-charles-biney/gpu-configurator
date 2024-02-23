45fbbc7d41acddbd5f978cbd1204daadc26de8cb
Joseph Ampadu <joseph.ampadu@amalitech.com>
2024-02-23 09:54:48 +0000

    fix: cors bean

--- src/main/java/com/amalitech/gpuconfigurator/config/SecurityConfig.java
+++ src/main/java/com/amalitech/gpuconfigurator/config/SecurityConfig.java
@@ -11,6 +11,12 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 @Configuration
@@ -23,6 +29,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
+        http.cors(httpSecurityCorsConfigurer -> {
+        });
         http.authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/auth/**", "/api/v1/carts/**").permitAll()
                 .requestMatchers("/api/v1/admin/**").hasAnyAuthority("ADMIN")
                 .requestMatchers(HttpMethod.GET, "/api/v1/cases/**").permitAll()
@@ -39,4 +47,13 @@ public class SecurityConfig {
     }
 
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+//        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
--- src/main/java/com/amalitech/gpuconfigurator/interceptors/UserSessionInterceptor.java
+++ src/main/java/com/amalitech/gpuconfigurator/interceptors/UserSessionInterceptor.java
@@ -29,9 +29,9 @@ public class UserSessionInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        response.addHeader("Access-Control-Allow-Credentials", "true");
-        response.addHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-        response.addHeader("Vary", "Origin");
+//        response.addHeader("Access-Control-Allow-Credentials", "true");
+//        response.addHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+//        response.addHeader("Vary", "Origin");
 
         if (request.getCookies() == null) {
             createNewUserSessionCookie(request, response);
