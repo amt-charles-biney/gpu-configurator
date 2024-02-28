81e03a59c912038427784202ff651800c3e8ed20
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-02-28 17:37:01 +0000

    feat: added the payment service interface

--- src/main/java/com/amalitech/gpuconfigurator/service/payment/PaymentService.java
+++ src/main/java/com/amalitech/gpuconfigurator/service/payment/PaymentService.java
@@ -1,13 +1,15 @@
 package com.amalitech.gpuconfigurator.service.payment;
 
-import com.amalitech.gpuconfigurator.config.Paystack;
+import com.amalitech.gpuconfigurator.dto.GenericResponse;
 import com.amalitech.gpuconfigurator.dto.Payment.InitializePaymentRequest;
 import com.amalitech.gpuconfigurator.dto.Payment.InitializePaymentResponse;
-import com.amalitech.gpuconfigurator.dto.Payment.PaymentVerifiedResponse;
-import com.amalitech.gpuconfigurator.dto.Payment.PaymentVerifyRequest;
+import com.amalitech.gpuconfigurator.dto.Payment.VerifyPaymentRequest;
+import org.springframework.validation.annotation.Validated;
 
 public interface PaymentService {
 
     public InitializePaymentResponse initializePaymentTransaction(InitializePaymentRequest paymentRequest);
-    public PaymentVerifiedResponse verifyPayment(PaymentVerifyRequest paymentResponse);
+
+    Object verifyPaymentTransaction(@Validated VerifyPaymentRequest reference);
+    // public PaymentVerifiedResponse verifyPayment(PaymentVerifyRequest paymentResponse);
 }
