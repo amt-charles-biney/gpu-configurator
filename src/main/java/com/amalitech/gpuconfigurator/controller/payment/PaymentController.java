30453da9ca81021df3c1ad713d020c29c2429faf
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-02-28 17:37:25 +0000

    feat: implemented payment controller

--- src/main/java/com/amalitech/gpuconfigurator/controller/payment/PaymentController.java
+++ src/main/java/com/amalitech/gpuconfigurator/controller/payment/PaymentController.java
@@ -1,2 +1,30 @@
-package com.amalitech.gpuconfigurator.controller.payment;public class PaymentController {
+package com.amalitech.gpuconfigurator.controller.payment;
+
+import com.amalitech.gpuconfigurator.dto.GenericResponse;
+import com.amalitech.gpuconfigurator.dto.Payment.InitializePaymentRequest;
+import com.amalitech.gpuconfigurator.dto.Payment.InitializePaymentResponse;
+import com.amalitech.gpuconfigurator.dto.Payment.VerifyPaymentRequest;
+import com.amalitech.gpuconfigurator.service.payment.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping("/v1/payment")
+    public ResponseEntity<InitializePaymentResponse> initializePayment(@Validated @RequestBody InitializePaymentRequest paymentRequest) {
+       return ResponseEntity.ok(paymentService.initializePaymentTransaction(paymentRequest));
+    }
+
+    @GetMapping("/v1/payment/{reference}")
+    public ResponseEntity<Object> verifyPayment(@PathVariable String reference) {
+        VerifyPaymentRequest verifyPaymentRequest = new VerifyPaymentRequest(reference);
+        return ResponseEntity.ok(paymentService.verifyPaymentTransaction(verifyPaymentRequest));
+    }
 }
