e34b3830dd780b48b10418699b5671bf77676560
Clement Owireku-Bogyah <clement.owireku-bogyah@amalitech.com>
2024-02-28 12:34:18 +0000

    feat: added order table

---
+++ src/main/java/com/amalitech/gpuconfigurator/model/Order.java
@@ -0,0 +1,40 @@
+package com.amalitech.gpuconfigurator.model;
+
+import com.amalitech.gpuconfigurator.model.payment.Payment;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "Order")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private OrderType status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private List<Payment> payments;
+
+    @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/amalitech/gpuconfigurator/model/OrderType.java
@@ -0,0 +1,8 @@
+package com.amalitech.gpuconfigurator.model;
+
+public enum OrderType {
+    SHIPPED,
+    DELIVERED,
+    PENDING,
+    CANCELLED
+}
