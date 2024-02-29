39de3e4fa5e1be0197b79f8a95fa174b3d8326ff
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-02-29 16:20:32 +0000

    refactor: refactored payment service with updated change

--- src/main/java/com/amalitech/gpuconfigurator/service/payment/PaymentService.java
+++ src/main/java/com/amalitech/gpuconfigurator/service/payment/PaymentService.java
@@ -3,13 +3,18 @@ package com.amalitech.gpuconfigurator.service.payment;
 import com.amalitech.gpuconfigurator.dto.GenericResponse;
 import com.amalitech.gpuconfigurator.dto.Payment.InitializePaymentRequest;
 import com.amalitech.gpuconfigurator.dto.Payment.InitializePaymentResponse;
+import com.amalitech.gpuconfigurator.dto.Payment.PaymentObjectRequest;
 import com.amalitech.gpuconfigurator.dto.Payment.VerifyPaymentRequest;
+import com.amalitech.gpuconfigurator.model.payment.Payment;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.validation.annotation.Validated;
+
+import java.security.Principal;
 
 public interface PaymentService {
 
-    public InitializePaymentResponse initializePaymentTransaction(InitializePaymentRequest paymentRequest);
+    InitializePaymentResponse initializePaymentTransaction(InitializePaymentRequest paymentRequest);
+    GenericResponse verifyPaymentTransaction(@Validated VerifyPaymentRequest reference, Principal user) throws NoSuchFieldException, JsonProcessingException;
 
-    Object verifyPaymentTransaction(@Validated VerifyPaymentRequest reference);
-    // public PaymentVerifiedResponse verifyPayment(PaymentVerifyRequest paymentResponse);
+    Payment savePaymentTransaction(PaymentObjectRequest paymentObjectRequest, Principal user);
 }
--- src/main/java/com/amalitech/gpuconfigurator/service/payment/PaymentServiceImpl.java
+++ src/main/java/com/amalitech/gpuconfigurator/service/payment/PaymentServiceImpl.java
@@ -3,24 +3,36 @@ package com.amalitech.gpuconfigurator.service.payment;
 import com.amalitech.gpuconfigurator.dto.GenericResponse;
 import com.amalitech.gpuconfigurator.dto.Payment.InitializePaymentRequest;
 import com.amalitech.gpuconfigurator.dto.Payment.InitializePaymentResponse;
+import com.amalitech.gpuconfigurator.dto.Payment.PaymentObjectRequest;
 import com.amalitech.gpuconfigurator.dto.Payment.VerifyPaymentRequest;
+import com.amalitech.gpuconfigurator.model.payment.Payment;
+import com.amalitech.gpuconfigurator.repository.payment.PaymentRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.client.RestTemplate;
+
+import java.security.Principal;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
 
     private final RestTemplate restTemplate;
+    private final PaymentRepository paymentRepository;
+    private final ObjectMapper objectMapper;
 
-    @Value("${paystack.api.url")
+    @Value("${paystack.api.url}")
     private String API_URL;
-    @Value("${paystack.api.key")
+    @Value("${paystack.api.key}")
     private String API_KEY;
+
     @Override
     public InitializePaymentResponse initializePaymentTransaction(@Validated InitializePaymentRequest paymentRequest) {
 
@@ -29,14 +41,16 @@ public class PaymentServiceImpl implements PaymentService {
                 .amount(paymentRequest.amount() * 100)
                 .currency(paymentRequest.currency())
                 .email(paymentRequest.email())
-                .reference(paymentRequest.currency())
+                .reference(paymentRequest.reference())
+                .channels(paymentRequest.channels())
                 .build();
-        
+
         HttpHeaders paymentHeaders = new HttpHeaders();
         paymentHeaders.set("Authorization", "Bearer " + API_KEY);
         paymentHeaders.setContentType(MediaType.APPLICATION_JSON);
 
         HttpEntity<InitializePaymentRequest> requestEntity = new HttpEntity<>(paymentDetails, paymentHeaders);
+
         String paymentUrl = API_URL + "/transaction/initialize";
 
         ResponseEntity<InitializePaymentResponse> response = restTemplate.exchange(
@@ -51,7 +65,8 @@ public class PaymentServiceImpl implements PaymentService {
 
 
     @Override
-    public Object verifyPaymentTransaction(@Validated VerifyPaymentRequest reference) {
+    public GenericResponse verifyPaymentTransaction(@Validated VerifyPaymentRequest reference, Principal user) throws JsonProcessingException {
+
         HttpHeaders verifyPaymentHeaders = new HttpHeaders();
         verifyPaymentHeaders.set("Authorization", "Bearer " + API_KEY);
 
@@ -59,16 +74,42 @@ public class PaymentServiceImpl implements PaymentService {
 
         HttpEntity<?> requestEntity = new HttpEntity<>(verifyPaymentHeaders);
 
-        return restTemplate.exchange(
+        Object paymentResponse =  restTemplate.exchange(
                 verifyPaymentUrl,
                 HttpMethod.GET,
                 requestEntity,
                 Object.class
         ).getBody();
 
-        // get the body
+        PaymentObjectRequest paymentResponseJson = mapPaymentResponseToPayment(paymentResponse);
+
+        if(paymentResponseJson.status()) {
+            Payment paymentTransaction = savePaymentTransaction(paymentResponseJson, user);
+        }
+
         // update the payment table
         // update the order table
 
+        return new GenericResponse(200, "payment verified successful");
     }
+
+    @Override
+    public Payment savePaymentTransaction(PaymentObjectRequest paymentObjectRequest, Principal user) {
+
+        Payment makePayment = Payment
+                .builder()
+                .ref(paymentObjectRequest.data().reference())
+                .channel(paymentObjectRequest.data().channel())
+                .currency(paymentObjectRequest.data().currency())
+                .amount(paymentObjectRequest.data().amount())
+                .build();
+
+        return paymentRepository.save(makePayment);
+    }
+
+    public PaymentObjectRequest mapPaymentResponseToPayment(Object paymentResponse) throws IllegalArgumentException, JsonProcessingException {
+        String paymentResponseJson = objectMapper.writeValueAsString(paymentResponse);
+        return (PaymentObjectRequest) objectMapper.readValue(paymentResponseJson, PaymentObjectRequest.class);
+    }
+
 }
