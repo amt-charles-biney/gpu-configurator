468289210cc922c16bce5dcc3556b129ff2a9222
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-03-04 11:37:03 +0000

    fix: changed response dto to add least total responses

--- src/main/java/com/amalitech/gpuconfigurator/dto/categoryconfig/CompatibleOptionGetResponse.java
+++ src/main/java/com/amalitech/gpuconfigurator/dto/categoryconfig/CompatibleOptionGetResponse.java
@@ -7,5 +7,5 @@ import java.util.List;
 import java.util.Map;
 
 @Builder
-public record CompatibleOptionGetResponse(String name, String id, String thumbnail, List<CompatibleOptionResponseDto> config, VariantStockLeastDto inStock, double configPrice) {
+public record CompatibleOptionGetResponse(String name, String id, String thumbnail, List<CompatibleOptionResponseDto> config, Integer inStock, List<VariantStockLeastDto> totalLeastStocks, double configPrice) {
 }
