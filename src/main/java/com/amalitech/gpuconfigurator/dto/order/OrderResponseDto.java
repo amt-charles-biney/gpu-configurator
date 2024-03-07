c46775aa3cb08f47ec2e958fc1a57a83805f3e69
Clement Owireku-Bogyah <clement.owireku-bogyah@amalitech.com>
2024-03-07 14:40:04 +0000

    feat: getting all orders

--- src/main/java/com/amalitech/gpuconfigurator/dto/order/OrderResponseDto.java
+++ src/main/java/com/amalitech/gpuconfigurator/dto/order/OrderResponseDto.java
@@ -1,16 +1,18 @@
 package com.amalitech.gpuconfigurator.dto.order;
 
 import com.amalitech.gpuconfigurator.model.OrderType;
+import com.amalitech.gpuconfigurator.model.configuration.Configuration;
 import lombok.Builder;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Builder
 public record OrderResponseDto(
         String productCoverImage,
-        String productName,
+        Set<Configuration> products,
         UUID orderId,
         String customerName,
         String paymentMethod,
