8267cf4d58d4356c5f58d94b6c2f3013f411e559
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-03-11 16:37:54 +0000

    feat(minor): added encryption service

---
+++ src/main/java/com/amalitech/gpuconfigurator/service/encryption/AesEncryptionService.java
@@ -0,0 +1,63 @@
+package com.amalitech.gpuconfigurator.service.encryption;
+
+import org.springframework.stereotype.Service;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+@Service
+public class AesEncryptionService implements EncryptionService {
+
+    private final static String secretKey = "2b7e151628aed2a6abf7158809cf4f3c";
+    private final static String algorithm = "AES/CBC/PKCS5PADDING";
+
+
+    @Override
+    public String encrypt(String word) throws NoSuchPaddingException,
+            NoSuchAlgorithmException, InvalidAlgorithmParameterException,
+            InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+
+        SecretKey key = getSecretKey(secretKey);
+        IvParameterSpec iv = generateIv();
+
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        byte[] cipherText = cipher.doFinal(word.getBytes());
+        return Base64.getEncoder()
+                .encodeToString(cipherText);
+    }
+
+    @Override
+    public String decrypt(String encryptedWord) throws NoSuchPaddingException,
+            NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException,
+            IllegalBlockSizeException, BadPaddingException {
+
+        SecretKey key = getSecretKey(secretKey);
+        IvParameterSpec iv = generateIv();
+
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
+        byte[] plainText = cipher.doFinal(Base64.getDecoder()
+                .decode(encryptedWord));
+        return new String(plainText);
+    }
+
+    public static SecretKey getSecretKey(String secretKey) {
+        byte[] SECRET_KEY_BYTES = secretKey.getBytes(UTF_8);
+        return new SecretKeySpec(SECRET_KEY_BYTES, "AES");
+    }
+
+    public static IvParameterSpec generateIv() {
+        byte[] iv = new byte[16];
+        return new IvParameterSpec(iv);
+    }
+
+}
