6171cc40ca28e6cc3f11a6b6d8695cd7c20b5b65
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-03-11 16:38:10 +0000

    feat(minor): added payment info repository

--- src/main/java/com/amalitech/gpuconfigurator/repository/PaymentInfoRepository/PaymentInfoRepository.java
+++ src/main/java/com/amalitech/gpuconfigurator/repository/PaymentInfoRepository/PaymentInfoRepository.java
@@ -1,11 +1,8 @@
 package com.amalitech.gpuconfigurator.repository.PaymentInfoRepository;
 
-import com.amalitech.gpuconfigurator.dto.PaymentInfo.CardInfoResponse;
-import com.amalitech.gpuconfigurator.dto.PaymentInfo.MobileMoneyResponse;
-import com.amalitech.gpuconfigurator.model.PaymentInfo.CardPayment;
-import com.amalitech.gpuconfigurator.model.PaymentInfo.MobilePayment;
 import com.amalitech.gpuconfigurator.model.PaymentInfo.PaymentInfo;
 import com.amalitech.gpuconfigurator.model.PaymentInfo.PaymentInfoType;
+import com.amalitech.gpuconfigurator.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,6 +11,5 @@ import java.util.UUID;
 
 @Repository
 public interface PaymentInfoRepository extends JpaRepository<PaymentInfoType, UUID> {
-    List<MobilePayment> findAllByMobilePayment(PaymentInfo paymentInfo);
-    List<CardPayment> findAllByCardPayment(PaymentInfo paymentInfo);
+    List<PaymentInfoType> findAllByPaymentTypeAndUser(PaymentInfo paymentInfo, User user);
 }
