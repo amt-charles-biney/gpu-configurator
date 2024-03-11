d7edb2173b32c8d84c18164d8c8c0de2158a3e20
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-03-11 16:38:20 +0000

    feat(minor): added payment info service

--- src/main/java/com/amalitech/gpuconfigurator/service/PaymentInfo/PaymentInfoService.java
+++ src/main/java/com/amalitech/gpuconfigurator/service/PaymentInfo/PaymentInfoService.java
@@ -2,18 +2,25 @@ package com.amalitech.gpuconfigurator.service.PaymentInfo;
 
 import com.amalitech.gpuconfigurator.dto.PaymentInfo.CardInfoRequest;
 import com.amalitech.gpuconfigurator.dto.PaymentInfo.CardInfoResponse;
+import com.amalitech.gpuconfigurator.dto.PaymentInfo.MobileMoneyRequest;
 import com.amalitech.gpuconfigurator.dto.PaymentInfo.MobileMoneyResponse;
+import com.amalitech.gpuconfigurator.model.PaymentInfo.PaymentInfoType;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 public interface PaymentInfoService {
-    List<MobileMoneyResponse> getAllMobileMoneyPayments();
+    List<PaymentInfoType> getAllMobileMoneyPayments();
 
-    MobileMoneyResponse saveMobileMoneyPayment(String phoneNumber);
+    MobileMoneyResponse saveMobileMoneyPayment(MobileMoneyRequest mobileMoneyRequest);
 
-    List<CardInfoResponse> getAllCardPayments();
+    List<PaymentInfoType> getAllCardPayments();
 
-    CardInfoResponse saveCardPayment(CardInfoRequest cardInfoRequest);
-
+    CardInfoResponse saveCardPayment(CardInfoRequest cardInfoRequest) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException;
 }
--- src/main/java/com/amalitech/gpuconfigurator/service/PaymentInfo/PaymentInfoServiceImpl.java
+++ src/main/java/com/amalitech/gpuconfigurator/service/PaymentInfo/PaymentInfoServiceImpl.java
@@ -2,14 +2,27 @@ package com.amalitech.gpuconfigurator.service.PaymentInfo;
 
 import com.amalitech.gpuconfigurator.dto.PaymentInfo.CardInfoRequest;
 import com.amalitech.gpuconfigurator.dto.PaymentInfo.CardInfoResponse;
+import com.amalitech.gpuconfigurator.dto.PaymentInfo.MobileMoneyRequest;
 import com.amalitech.gpuconfigurator.dto.PaymentInfo.MobileMoneyResponse;
 import com.amalitech.gpuconfigurator.model.PaymentInfo.CardPayment;
 import com.amalitech.gpuconfigurator.model.PaymentInfo.MobilePayment;
 import com.amalitech.gpuconfigurator.model.PaymentInfo.PaymentInfo;
+import com.amalitech.gpuconfigurator.model.PaymentInfo.PaymentInfoType;
+import com.amalitech.gpuconfigurator.model.User;
 import com.amalitech.gpuconfigurator.repository.PaymentInfoRepository.PaymentInfoRepository;
+import com.amalitech.gpuconfigurator.service.encryption.AesEncryptionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -17,48 +30,67 @@ import java.util.List;
 public class PaymentInfoServiceImpl implements PaymentInfoService{
 
     private final PaymentInfoRepository paymentInfoRepository;
+    private final AesEncryptionService encryptionService;
 
     @Override
-    public List<MobileMoneyResponse> getAllMobileMoneyPayments() {
-        List<MobilePayment> mobilePayments = paymentInfoRepository.findAllByMobilePayment(PaymentInfo.MOBILE_MONEY);
-        return mobilePayments.stream()
-                .map(payment -> new MobileMoneyResponse(payment.getPhoneNumber(), payment.getId().toString()))
-                .toList();
+    public List<PaymentInfoType> getAllMobileMoneyPayments() {
+        User user = getCurrentUserHelper();
+       return paymentInfoRepository.findAllByPaymentTypeAndUser(PaymentInfo.MOBILE_MONEY, user);
     }
 
     @Override
-    public MobileMoneyResponse saveMobileMoneyPayment(String phoneNumber) {
+    public List<PaymentInfoType> getAllCardPayments() {
+        User user = getCurrentUserHelper();
+        return paymentInfoRepository.findAllByPaymentTypeAndUser(PaymentInfo.CARD, user);
+    }
+
+    @Override
+    public MobileMoneyResponse saveMobileMoneyPayment(@Validated MobileMoneyRequest mobileMoneyRequest) {
+        User user = getCurrentUserHelper();
         MobilePayment paymentInfoType = new MobilePayment();
+
+        paymentInfoType.setPhoneNumber(mobileMoneyRequest.phoneNumber());
         paymentInfoType.setPaymentType(PaymentInfo.MOBILE_MONEY);
+        paymentInfoType.setUser(user);
+
         MobilePayment mobilePayment = paymentInfoRepository.save(paymentInfoType);
 
         return new MobileMoneyResponse(mobilePayment.getPhoneNumber(), mobilePayment.getId().toString());
     }
 
     @Override
-    public List<CardInfoResponse> getAllCardPayments() {
-        return paymentInfoRepository.findAllByCardPayment(PaymentInfo.CARD).stream()
-                .map(payment -> CardInfoResponse
-                        .builder()
-                        .cardHolderName(payment.getCardholderName())
-                        .cardNumber(payment.getCardNumber())
-                        .expirationDate(payment.getExpirationDate())
-                        .build())
-                .toList();
-    }
+    public CardInfoResponse saveCardPayment(CardInfoRequest cardInfoRequest) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+        User user = getCurrentUserHelper();
 
-    @Override
-    public CardInfoResponse saveCardPayment(CardInfoRequest cardInfoRequest) {
         CardPayment cardPayment = new CardPayment();
         cardPayment.setPaymentType(PaymentInfo.CARD);
+
+        String encryptedCardNumber = encryptionService.encrypt(cardInfoRequest.cardNumber());
+        String encryptedCardHolderName = encryptionService.encrypt(cardInfoRequest.cardHolderName());
+        String encryptedCardExpiry = encryptionService.encrypt(cardInfoRequest.expirationDate());
+
+        cardPayment.setCardNumber(encryptedCardNumber);
+        cardPayment.setCardholderName(encryptedCardHolderName);
+        cardPayment.setExpirationDate(encryptedCardExpiry);
+        cardPayment.setUser(user);
+
         CardPayment savedCardPayment = paymentInfoRepository.save(cardPayment);
 
         return CardInfoResponse
                 .builder()
                 .id(savedCardPayment.getId().toString())
-                .cardHolderName(savedCardPayment.getCardholderName())
+                .cardholderName(encryptionService.decrypt(savedCardPayment.getCardholderName()))
                 .expirationDate(savedCardPayment.getExpirationDate())
                 .cardNumber(savedCardPayment.getCardNumber())
                 .build();
+    }
+
+    public User getCurrentUserHelper() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(!authentication.isAuthenticated())
+            throw new IllegalArgumentException("No authenticated user found");
+
+        return (User) authentication.getPrincipal();
     }
 }
