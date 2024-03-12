4205e25c7438b04b85374d5f7b7240791daab67a
Clement Owireku-Bogyah <clement.owireku-bogyah@amalitech.com>
2024-03-12 08:53:50 +0000

    testing

--- src/main/java/com/amalitech/gpuconfigurator/service/payment/PaymentService.java
+++ src/main/java/com/amalitech/gpuconfigurator/service/payment/PaymentService.java
@@ -5,6 +5,10 @@ import com.amalitech.gpuconfigurator.dto.Payment.*;
 import com.amalitech.gpuconfigurator.model.UserSession;
 import com.amalitech.gpuconfigurator.model.payment.Payment;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.shippo.exception.APIConnectionException;
+import com.shippo.exception.APIException;
+import com.shippo.exception.AuthenticationException;
+import com.shippo.exception.InvalidRequestException;
 import org.springframework.validation.annotation.Validated;
 
 import java.security.Principal;
@@ -12,6 +16,6 @@ import java.security.Principal;
 public interface PaymentService {
 
     InitializePaymentResponse initializePaymentTransaction(InitializePaymentRequest paymentRequest);
-    VerifyPaymentResponse verifyPaymentTransaction(@Validated VerifyPaymentRequest reference, Principal user, UserSession userSession) throws NoSuchFieldException, JsonProcessingException;
+    VerifyPaymentResponse verifyPaymentTransaction(@Validated VerifyPaymentRequest reference, Principal user, UserSession userSession) throws NoSuchFieldException, JsonProcessingException, APIConnectionException, APIException, AuthenticationException, InvalidRequestException;
     Payment savePaymentTransaction(PaymentObjectRequest paymentObjectRequest, Principal principal, UserSession userSession);
 }
