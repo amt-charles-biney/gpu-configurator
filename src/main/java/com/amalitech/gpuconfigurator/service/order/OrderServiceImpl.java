fe6f1deee48f706d3059a968449c02b59a6522af
Clement Owireku-Bogyah <clement.owireku-bogyah@amalitech.com>
2024-03-12 09:12:15 +0000

    fix: pom

--- src/main/java/com/amalitech/gpuconfigurator/service/order/OrderServiceImpl.java
+++ src/main/java/com/amalitech/gpuconfigurator/service/order/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.amalitech.gpuconfigurator.service.order;
 
 
+import com.amalitech.gpuconfigurator.constant.ShippoContants;
 import com.amalitech.gpuconfigurator.dto.GenericResponse;
 import com.amalitech.gpuconfigurator.dto.order.CreateOrderDto;
 import com.amalitech.gpuconfigurator.dto.order.OrderResponseDto;
@@ -13,8 +14,17 @@ import com.amalitech.gpuconfigurator.model.*;
 import com.amalitech.gpuconfigurator.model.payment.Payment;
 import com.amalitech.gpuconfigurator.repository.UserRepository;
 import com.amalitech.gpuconfigurator.repository.UserSessionRepository;
+import com.shippo.Shippo;
+import com.shippo.exception.APIConnectionException;
+import com.shippo.exception.APIException;
+import com.shippo.exception.AuthenticationException;
+import com.shippo.exception.InvalidRequestException;
+import com.shippo.model.Rate;
+import com.shippo.model.Shipment;
+import com.shippo.model.Transaction;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 
 import org.springframework.data.domain.PageRequest;
@@ -25,8 +35,7 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.security.Principal;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 
 @Service
@@ -36,10 +45,16 @@ public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
     private final UserRepository userRepository;
     private final UserSessionRepository userSessionRepository;
+    @Value("${shippo-test-key}")
+    private String shippoTestKey;
 
     @Override
     @Transactional
-    public CreateOrderDto createOrder(Payment payment, Principal principal, UserSession userSession) {
+    public CreateOrderDto createOrder(Payment payment, Principal principal, UserSession userSession) throws APIConnectionException, APIException, AuthenticationException, InvalidRequestException {
+
+        Shippo.setApiKey(shippoTestKey);
+        Map<String, Object> toAddressMap = new HashMap<>();
+
 
         User user = null;
         UsernamePasswordAuthenticationToken authenticationToken = ((UsernamePasswordAuthenticationToken) principal);
@@ -48,22 +63,97 @@ public class OrderServiceImpl implements OrderService {
             user = (User) authenticationToken.getPrincipal();
         }
 
+
         Order.OrderBuilder orderBuilder = Order.builder();
 
         if (user != null) {
             orderBuilder.cart(user.getCart());
             user.setCart(null);
             userRepository.save(user);
+            toAddressMap.put("name", user.getFirstName());
+            toAddressMap.put("company", "Shippo");
+            toAddressMap.put("street1", user.getShippingInformation().getAddress1());
+            toAddressMap.put("city", user.getShippingInformation().getCity());
+            toAddressMap.put("state", user.getShippingInformation().getState());
+            toAddressMap.put("zip", user.getShippingInformation().getZipCode());
+            toAddressMap.put("country", user.getShippingInformation().getCountry());
+            toAddressMap.put("phone", "+1 555 341 9393");
+            toAddressMap.put("email", user.getShippingInformation().getEmail());
+
 
         } else {
             orderBuilder.cart(userSession.getCart());
             userSession.setCart(null);
             userSessionRepository.save(userSession);
+            toAddressMap.put("name", userSession.getCurrentShipping().getFirstName());
+            toAddressMap.put("company", "Shippo");
+            toAddressMap.put("street1", userSession.getCurrentShipping().getAddress1());
+            toAddressMap.put("city", userSession.getCurrentShipping().getCity());
+            toAddressMap.put("state", userSession.getCurrentShipping().getState());
+            toAddressMap.put("zip", userSession.getCurrentShipping().getZipCode());
+            toAddressMap.put("country", userSession.getCurrentShipping().getCountry());
+            toAddressMap.put("phone", "+1 555 341 9393");
+            toAddressMap.put("email", userSession.getCurrentShipping().getEmail());
 
         }
 
+
+        // from address
+        Map<String, Object> fromAddressMap = new HashMap<>();
+        fromAddressMap.put("name", ShippoContants.ADDRESS_FROM_COMPANY);
+        fromAddressMap.put("company", ShippoContants.ADDRESS_FROM_COMPANY);
+        fromAddressMap.put("street1", ShippoContants.ADDRESS_FROM);
+        fromAddressMap.put("city", ShippoContants.ADDRESS_FROM_CITY);
+        fromAddressMap.put("state", ShippoContants.ADDRESS_FROM_STATE);
+        fromAddressMap.put("zip", ShippoContants.ADDRESS_FROM_ZIP);
+        fromAddressMap.put("country", ShippoContants.ADDRESS_FROM_COUNTRY);
+        fromAddressMap.put("email", ShippoContants.ADDRESS_FROM_EMAIL);
+        fromAddressMap.put("phone", ShippoContants.ADDRESS_FROM_PHONE);
+        fromAddressMap.put("metadata", ShippoContants.ADDRESS_FROM_ID);
+
+//        Map<String, Object> fromAddressMap = new HashMap<>();
+//        fromAddressMap.put("name", "Ms Hippo");
+//        fromAddressMap.put("company", "San Diego Zoo");
+//        fromAddressMap.put("street1", "2920 Zoo Drive");
+//        fromAddressMap.put("city", "San Diego");
+//        fromAddressMap.put("state", "CA");
+//        fromAddressMap.put("zip", "92101");
+//        fromAddressMap.put("country", "US");
+//        fromAddressMap.put("email", "mshippo@goshipppo.com");
+//        fromAddressMap.put("phone", "+1 619 231 1515");
+//        fromAddressMap.put("metadata", "Customer ID 123456");
+
+        // parcel
+        Map<String, Object> parcelMap = new HashMap<>();
+        parcelMap.put("length", "5");
+        parcelMap.put("width", "5");
+        parcelMap.put("height", "5");
+        parcelMap.put("distance_unit", "in");
+        parcelMap.put("weight", "2");
+        parcelMap.put("mass_unit", "lb");
+        List<Map<String, Object>> parcels = new ArrayList<>();
+        parcels.add(parcelMap);
+
+        Map<String, Object> shipmentMap = new HashMap<>();
+        shipmentMap.put("address_to", toAddressMap);
+        shipmentMap.put("address_from", fromAddressMap);
+        shipmentMap.put("parcels", parcels);
+        shipmentMap.put("async", false);
+
+        Shipment shipment = Shipment.create(shipmentMap);
+
+        List<Rate> rates = shipment.getRates();
+        Rate rate = rates.getFirst();
+
+        Map<String, Object> transParams = new HashMap<>();
+        transParams.put("rate", rate.getObjectId());
+        transParams.put("async", false);
+        Transaction transaction = Transaction.create(transParams);
+
+
         Order order = orderBuilder
-                .status(OrderType.PENDING)
+                .tracking_id((String) transaction.getTrackingNumber())
+                .status((String) transaction.getTrackingStatus())
                 .user(user)
                 .payment(payment).build();
         orderRepository.save(order);
@@ -79,6 +169,7 @@ public class OrderServiceImpl implements OrderService {
 
         return orderRepository.findAll(pageable).map(this::mapOrderToOrderResponseDto);
     }
+
 
     @Override
     public OrderResponseDto getOrderById(UUID id) {
