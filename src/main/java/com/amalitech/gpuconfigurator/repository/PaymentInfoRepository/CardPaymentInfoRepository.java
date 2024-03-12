f256566003e076b5f93e6decd7f9d9b2a448af3f
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-03-12 11:54:21 +0000

    refactor: updated payment info repositories(MobileMoney, CardPayment)

--- src/main/java/com/amalitech/gpuconfigurator/repository/PaymentInfoRepository/CardPaymentInfoRepository.java
+++ src/main/java/com/amalitech/gpuconfigurator/repository/PaymentInfoRepository/CardPaymentInfoRepository.java
@@ -5,11 +5,11 @@ import com.amalitech.gpuconfigurator.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CardPaymentInfoRepository extends JpaRepository<CardPayment, UUID> {
 
-    List<CardPayment> findAllByUser(User user);
+    Optional<CardPayment> findOneByUser(User user);
 }
