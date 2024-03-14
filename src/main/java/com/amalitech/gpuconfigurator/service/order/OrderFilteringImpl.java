c42cf3cadb7776769891928f8e0efcd54681d232
Clement Owireku-Bogyah <clement.owireku-bogyah@amalitech.com>
2024-03-14 07:47:10 +0000

    fix: reset sgtatus filter

--- src/main/java/com/amalitech/gpuconfigurator/service/order/OrderFilteringImpl.java
+++ src/main/java/com/amalitech/gpuconfigurator/service/order/OrderFilteringImpl.java
@@ -44,7 +44,7 @@ public class OrderFilteringImpl implements OrderFiltering {
     }
 
     private Specification<Order> applyStatusFilter(Specification<Order> spec, String status) {
-        if (status != null && !"all".equals(status) && !status.isEmpty()) {
+        if (status != null && !"all".equalsIgnoreCase(status) && !status.isEmpty()) {
             spec = spec.and((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("status"), status));
         }
         return spec;
