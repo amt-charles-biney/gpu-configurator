aa01b012cd72993931c887476dbebb8041512826
Clement Owireku-Bogyah <clement.owireku-bogyah@amalitech.com>
2024-04-03 09:04:42 +0000

    fix: shipment dto

--- src/main/java/com/amalitech/gpuconfigurator/dto/ShipmentDto.java
+++ src/main/java/com/amalitech/gpuconfigurator/dto/ShipmentDto.java
@@ -3,12 +3,14 @@ package com.amalitech.gpuconfigurator.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class ShipmentDto {
     private UUID id;
 }
