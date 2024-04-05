db03377451f658b3a6c65bbdf6da6ca2eddc25ac
dickson anyaele <dicksonanyaele1234@gmail.com>
2024-04-05 08:53:11 +0000

    fix: cors|

--- src/main/java/com/amalitech/gpuconfigurator/config/SecurityConfig.java
+++ src/main/java/com/amalitech/gpuconfigurator/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,6 +25,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
+        http.cors(Customizer.withDefaults());
         http.authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/auth/**", "/api/v1/carts/**", "/api/v1/shipping/**").permitAll()
                 .requestMatchers("api/v1/wishlists/**").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/v1/webhook/easypost").permitAll()
