03fdc9e9faa8000d0c928c720f6999863e890aea
Joseph Ampadu <joseph.ampadu@amalitech.com>
2024-04-08 15:08:36 +0000

    fix: trim request params

--- src/main/java/com/amalitech/gpuconfigurator/controller/ProductController.java
+++ src/main/java/com/amalitech/gpuconfigurator/controller/ProductController.java
@@ -98,10 +98,10 @@ public class ProductController {
     ) {
         ProductSearchRequest productSearchRequest = ProductSearchRequest.builder()
                 .query(query)
-                .cases(productCase.stream().map(String::strip).toList())
+                .cases(productCase.stream().map(String::trim).toList())
                 .prices(price)
-                .brands(brand.stream().map(String::strip).toList())
-                .categories(categories.stream().map(String::strip).toList())
+                .brands(brand.stream().map(String::trim).toList())
+                .categories(categories.stream().map(String::trim).toList())
                 .build();
 
         User user = principal == null ? null : (User) ((UsernamePasswordAuthenticationToken) principal).getPrincipal();
