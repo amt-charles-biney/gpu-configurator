886a9c8c6c7169b7fa7dff4835180848a79f59b7
Dickson Anyaele <dickson.anyaele@amalitech.com>
2024-04-11 22:36:45 +0000

    fix notifications service updated and addad repository tag to attribute

--- src/main/java/com/amalitech/gpuconfigurator/repository/attribute/AttributeOptionRepository.java
+++ src/main/java/com/amalitech/gpuconfigurator/repository/attribute/AttributeOptionRepository.java
@@ -2,11 +2,13 @@ package com.amalitech.gpuconfigurator.repository.attribute;
 
 import com.amalitech.gpuconfigurator.model.attributes.AttributeOption;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface AttributeOptionRepository extends JpaRepository<AttributeOption, UUID> {
     Optional<AttributeOption> findByOptionName(String name);
     Optional<List<AttributeOption>> findAllByAttributeId(UUID id);
